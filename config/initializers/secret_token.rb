44cb8f2a256c25d05d66469550b7847a6b70b956
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-02-12 19:15:46 +0900

    Add rails-template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTemplate::Application.config.secret_token = '129d9ce1ec53f0b9bdc88ea8d936d4f4b1754a76fc87b78620c2505feb04fa0b20ff6ebbc0b6a8b2fb747ae05ac124fe554d6f7d73319040d605b6779fcb083f'
